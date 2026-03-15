e0ab8afd02134367666a4f1abff9b51c65cdb33b
D Anwar Baba <annuanwar225@gmail.com>
2026-03-15 17:07:22 +0530

    Fixing the erros

--- src/main/java/com/job/JobBackendApplication.java
+++ src/main/java/com/job/JobBackendApplication.java
@@ -1,13 +1,11 @@
 package com.job;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class JobBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JobBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JobBackendApplication.class, args);
+    }
 }
